9e71dbfd89206b123657be501d8656bd58263304
jaxonhill <jaxoncharleshill@gmail.com>
2023-10-31 20:17:17 -0700

    Add diversity course options dictionary

--- main.py
+++ main.py
@@ -23,7 +23,34 @@ def main() -> None:
 
     # Filter course spans to get diversity classes with the "+" at the end of the number
     diversity_course_spans: list[Tag] = list(filter(filter_condition, course_spans))
-    pprint.pprint(diversity_course_spans)
+
+    # Create dictionary of departments and numbers
+    course_department_number_dict: dict = {}
+    for span in diversity_course_spans:
+        # Get current department and class number
+        department: str = span["department"]
+        curr_class_number: str = span["number"].replace("+", "")
+
+        # If the department already exists in the dictionary, append to the array
+        if department in course_department_number_dict.keys():
+            course_department_number_dict[department].append(curr_class_number)
+        else:
+            course_department_number_dict[department] = [curr_class_number]
+
+    pprint.pprint(course_department_number_dict)
+
+    # TODO:
+    # Open selenium
+    # Click Spring 2024
+    # Navigate based on department dictionary using URL f-string
+    # Match course names and open them to get class options
+    # Store all info for each class option using pandas
+    # Add a "requirement", is it Explorations, CS Elective, CS Core Class?
+    # Get RateMyProfessor ratings
+    # Match up professor names
+    # Get ratings, might be multiple depending on multiple professors, so create an average rating
+    # Sort by average rating
+    # Filter out classes that are online?
 
 
 if __name__ == "__main__":
